16e90d36c836e313ea7bec435ec615a260be2c4a
jogspokoen88 <jogspokoen88@mail.ru>
2020-07-27 13:30:09 +0500

    Updates

---
+++ precache-manifest.fcbf568dfbe35837345d01b9c6408bc2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bb7da31f4ff73b05a0b",
+    "url": "/Game-of-Thrones/static/css/main.ebf814fd.chunk.css"
+  },
+  {
+    "revision": "1bb7da31f4ff73b05a0b",
+    "url": "/Game-of-Thrones/static/js/main.1bb7da31.chunk.js"
+  },
+  {
+    "revision": "6744280d8661dc518de2",
+    "url": "/Game-of-Thrones/static/css/1.3ef8d3b7.chunk.css"
+  },
+  {
+    "revision": "6744280d8661dc518de2",
+    "url": "/Game-of-Thrones/static/js/1.6744280d.chunk.js"
+  },
+  {
+    "revision": "79d527da840808d580b1",
+    "url": "/Game-of-Thrones/static/js/runtime~main.79d527da.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/Game-of-Thrones/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/Game-of-Thrones/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "10e5f4c6214c9458e59170e04949c1f7",
+    "url": "/Game-of-Thrones/index.html"
+  }
+];
